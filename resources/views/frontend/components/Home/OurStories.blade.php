d08b8aedf48093f3581e55be993e81e152f06b92
loomoni <loomonimorwo1@gmail.com>
2024-05-31 12:04:40 +0300

    ourstories modified

--- resources/views/frontend/components/Home/OurStories.blade.php
+++ resources/views/frontend/components/Home/OurStories.blade.php
@@ -7,7 +7,7 @@
                 <div id="who-we-are">
                     <div class="row g-5 h-100" data-aos="flip-left" data-aos-once="false">
                         <div class="col-lg-6 d-flex align-items-start flex-column justify-content-start text-dinim" data-aos="fade-right" data-aos-once="false">
-                            <div class="d-flex flex-column align-items-end">
+                            <div class="d-flex flex-column align-items-end" style="padding-top: 3rem">
                                 <div class="d-flex  align-items-center justify-content-center w-75">
                                     <div class="text-dinim text-center about-title">
                                         <h2><strong>Our Story</strong></h2>
@@ -22,7 +22,12 @@
                                     </p>
                                     
                                     <p class="story-title">
-                                        Established over 2 decades ago, FCS is the largest local
+                                        FCS is the largest local
+                                        grantmaker with extensive
+                                        reach and deep roots within
+                                        the grassroots. Since its
+                                        inception, FCS has grown
+                                        {{-- Established over 2 decades ago, FCS is the largest local
                                         grant maker with extensive 
                                     
                                         Established over 2 decades ago, FCS is the largest local
@@ -35,7 +40,7 @@
                                         grant maker with extensive 
 
                                         Established over 2 decades ago, FCS is the largest local
-                                        grant maker with extensive 
+                                        grant maker with extensive  --}}
                                         
                                     </p>
                                 </div>
@@ -50,13 +55,18 @@
                         <div class="col-lg-6 pt-4 d-flex  align-items-center pt-lg-0" data-aos="fade-left" data-aos-once="false">
                             <div class="d-flex flex-column story-left w-75">
                                 <p class="independent-title mt-2 text-dinim">
-                                    Established over 2 decades ago, FCS is the largest local
-                                    grant maker with extensive Established over 2 decades ago, FCS is the largest local
-                                    grant maker with extensive Established over 2 decades ago, FCS is the largest local
-                                    grant maker with extensive Established over 2 decades ago, FCS is the largest local
-                                    grant maker with extensive 
-                                    Established over 2 decades ago, FCS is the largest local
-                                    grant maker with extensive 
+                                    Over two decades ago, in the heart of Tanzania's
+                                    development landscape, a remarkable transformation
+                                    began. It was a time when a collective desire was
+                                    growing, one that sought to place citizens at the heart of
+                                    their nation's transformation. The prevailing wisdom held
+                                    that authentic development could only flourish when
+                                    individuals were not mere observers but active masters
+                                    of their destinies. Yet, this noble aspiration was faced
+                                    with a challenge – international donors, while
+                                    well-intentioned, often struggled to harmonize with the
+                                    local tapestry of culture and also siphon funds straight to
+                                    CSOs due to their infancy
                                 </p>
                                 <div class="image-experience">
                                     <img src="{{ url('frontend/images/fcs_women.png') }}" class="img-fluid-fcs" alt="">
